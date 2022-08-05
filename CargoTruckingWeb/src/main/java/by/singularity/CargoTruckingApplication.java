576996be5e0ebe0abfa60a015fcc9a7858e5356b
AlexandrKaminskiy <sasha.pinsk2003@gmail.com>
2022-08-05 23:40:48 +0300

    Initial commit

---
+++ CargoTruckingWeb/src/main/java/by/singularity/CargoTruckingApplication.java
@@ -0,0 +1,12 @@
+package by.singularity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CargoTruckingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CargoTruckingApplication.class);
+    }
+}
