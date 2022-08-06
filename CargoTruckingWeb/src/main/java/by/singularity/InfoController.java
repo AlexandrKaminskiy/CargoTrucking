6ad78a6a75f4299cb5f6ec286e8dff3aedaa9b21
AlexandrKaminskiy <sasha.pinsk2003@gmail.com>
2022-08-06 21:13:07 +0300

    Added cors

--- CargoTruckingWeb/src/main/java/by/singularity/InfoController.java
+++ CargoTruckingWeb/src/main/java/by/singularity/InfoController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
+        System.out.println("hello");
         return "hello";
     }
 }
