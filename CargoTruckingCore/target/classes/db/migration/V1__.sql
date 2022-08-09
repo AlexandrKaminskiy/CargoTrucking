82e7d9e470ace9478475db72c2474f52ead9ad7e
AlexandrKaminskiy <sasha.pinsk2003@gmail.com>
2022-08-10 00:36:01 +0300

    Added some entities

--- CargoTruckingCore/target/classes/db/migration/V1__.sql
+++ CargoTruckingCore/target/classes/db/migration/V1__.sql
@@ -1,7 +1,28 @@
-CREATE SEQUENCE IF NOT EXISTS client_seq START WITH 1 INCREMENT BY 50;
-
 CREATE TABLE client
 (
-    id BIGINT NOT NULL,
+    id           BIGINT NOT NULL,
+    name         VARCHAR(20),
+    surname      VARCHAR(20),
+    patronymic   VARCHAR(20),
+    date         TIMESTAMP WITHOUT TIME ZONE,
+    email        VARCHAR(50),
+    town         VARCHAR(20),
+    street       VARCHAR(20),
+    house        INTEGER,
+    flat         INTEGER,
+    login        VARCHAR(20),
+    password     VARCHAR(72),
+    passport_num VARCHAR(30),
+    issued_by    VARCHAR(50),
+    user_roles   VARCHAR(255),
     CONSTRAINT pk_client PRIMARY KEY (id)
+);
+
+CREATE TABLE storage
+(
+    id        BIGINT NOT NULL,
+    name      VARCHAR(20),
+    address   VARCHAR(20),
+    client_id BIGINT NOT NULL,
+    CONSTRAINT pk_storage PRIMARY KEY (id)
 );
